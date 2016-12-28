e826cd7bec83590a097e1b7f276b276d40d41488
mjhead <andremjhead@gmail.com>
2016-12-28 15:33:13 +0200

    UPD: https://github.com/CherryFramework/cherry-services-list/issues/4

--- public/includes/class-cherry-services-list-shortcode.php
+++ public/includes/class-cherry-services-list-shortcode.php
@@ -70,147 +70,6 @@ class Cherry_Services_List_Shortcode {
 	}
 
 	/**
-	 * Filter to modify original shortcodes data and add [$this->name] shortcode.
-	 *
-	 * @since  1.0.0
-	 * @param  array $shortcodes Original plugin shortcodes.
-	 * @return array             Modified array.
-	 */
-	public function shortcodes( $shortcodes ) {
-
-		$terms_list = array();
-
-		if ( did_action( 'wp_ajax_cherry_shortcodes_generator_settings' ) ) {
-			$terms = get_terms( 'group' );
-
-			if ( ! is_wp_error( $terms ) ) {
-				$terms_list = wp_list_pluck( $terms, 'name', 'slug' );
-			}
-		}
-
-		$sizes_list = array();
-		if ( class_exists( 'Cherry_Shortcodes_Tools' ) && method_exists( 'Cherry_Shortcodes_Tools', 'image_sizes' ) ) {
-			$sizes_list = Cherry_Shortcodes_Tools::image_sizes();
-		}
-
-		$shortcodes[ self::$name ] = array(
-			'name'  => __( 'Team', 'cherry-services' ), // Shortcode name.
-			'desc'  => __( 'Cherry services shortcode', 'cherry-services' ),
-			'type'  => 'single', // Can be 'wrap' or 'single'. Example: [b]this is wrapped[/b], [this_is_single]
-			'group' => 'content', // Can be 'content', 'box', 'media' or 'other'. Groups can be mixed
-			'atts'  => array( // List of shortcode params (attributes).
-				'limit' => array(
-					'type'    => 'slider',
-					'min'     => -1,
-					'max'     => 100,
-					'step'    => 1,
-					'default' => 3,
-					'name'    => __( 'Limit', 'cherry-services' ),
-					'desc'    => __( 'Maximum number of posts.', 'cherry-services' ),
-				),
-				'order' => array(
-					'type' => 'select',
-					'values' => array(
-						'desc' => __( 'Descending', 'cherry-services' ),
-						'asc'  => __( 'Ascending', 'su' ),
-					),
-					'default' => 'DESC',
-					'name' => __( 'Order', 'cherry-services' ),
-					'desc' => __( 'Posts order', 'cherry-services' ),
-				),
-				'orderby' => array(
-					'type' => 'select',
-					'values' => array(
-						'none'          => __( 'None', 'cherry-services' ),
-						'id'            => __( 'Post ID', 'cherry-services' ),
-						'author'        => __( 'Post author', 'cherry-services' ),
-						'title'         => __( 'Post title', 'cherry-services' ),
-						'name'          => __( 'Post slug', 'cherry-services' ),
-						'date'          => __( 'Date', 'cherry-services' ),
-						'modified'      => __( 'Last modified date', 'cherry-services' ),
-						'rand'          => __( 'Random', 'cherry-services' ),
-						'comment_count' => __( 'Comments number', 'cherry-services' ),
-						'menu_order'    => __( 'Menu order', 'cherry-services' ),
-					),
-					'default' => 'date',
-					'name'    => __( 'Order by', 'cherry-services' ),
-					'desc'    => __( 'Order posts by', 'cherry-services' ),
-				),
-				'category' => array(
-					'type'     => 'select',
-					'multiple' => true,
-					'values'   => $terms_list,
-					'default'  => '',
-					'name'     => __( 'Category', 'cherry-services' ),
-					'desc'     => __( 'Select groups to show services members from', 'cherry-services' ),
-				),
-				'id' => array(
-					'default' => 0,
-					'name'    => __( 'Post ID\'s', 'cherry-services' ),
-					'desc'    => __( 'Enter comma separated ID\'s of the posts that you want to show', 'cherry-services' ),
-				),
-				'show_title' => array(
-					'type'    => 'bool',
-					'default' => 'yes',
-					'name' => __( 'Show name?', 'cherry-services' ),
-					'desc'    => __( 'Show name?', 'cherry-services' ),
-				),
-				'show_media' => array(
-					'type'    => 'bool',
-					'default' => 'yes',
-					'name' => __( 'Show photo?', 'cherry-services' ),
-					'desc'    => __( 'Show photo?', 'cherry-services' ),
-				),
-				'size' => array(
-					'type'    => 'select',
-					'values'  => $sizes_list,
-					'default' => 'thumbnail',
-					'name'    => __( 'Featured image size', 'cherry-services' ),
-					'desc'    => __( 'Select size for a Featured image', 'cherry-services' ),
-				),
-				'excerpt_length' => array(
-					'type'    => 'slider',
-					'min'     => 5,
-					'max'     => 150,
-					'step'    => 1,
-					'default' => 20,
-					'name'    => __( 'Excerpt Length', 'cherry-services' ),
-					'desc'    => __( 'Excerpt length (if used in template)', 'cherry-services' ),
-				),
-				'col' => array(
-					'type'    => 'responsive',
-					'default' => array(
-						'col_xs' => 'none',
-						'col_sm' => 'none',
-						'col_md' => 'none',
-						'col_lg' => 'none',
-					),
-					'name'    => __( 'Column class', 'cherry-services' ),
-					'desc'    => __( 'Column class for each item.', 'cherry-services' ),
-				),
-				'template' => array(
-					'type'   => 'select',
-					'values' => array(
-						'default.tmpl' => 'default.tmpl',
-					),
-					'default' => 'default.tmpl',
-					'name'    => __( 'Template', 'cherry-services' ),
-					'desc'    => __( 'Shortcode template', 'cherry-services' ),
-				),
-				'class' => array(
-					'default' => '',
-					'name'    => __( 'Class', 'cherry-services' ),
-					'desc'    => __( 'Extra CSS class', 'cherry-services' ),
-				),
-			),
-			'icon'     => 'users', // Custom icon (font-awesome).
-			'function' => array( $this, 'do_shortcode' ), // Name of shortcode function.
-		);
-
-		return $shortcodes;
-	}
-
-	/**
 	 * The shortcode function.
 	 *
 	 * @since  1.0.0
@@ -227,6 +86,7 @@ class Cherry_Services_List_Shortcode {
 			'title'          => '',
 			'subtitle'       => '',
 			'columns'        => 3,
+			'columns_laptop' => 3,
 			'columns_tablet' => 2,
 			'columns_phone'  => 1,
 			'posts_per_page' => 6,
@@ -271,7 +131,7 @@ class Cherry_Services_List_Shortcode {
 		$col_classes = '';
 
 		// Fix columns
-		foreach ( array( 'columns', 'columns_tablet', 'columns_phone' ) as $col ) {
+		foreach ( array( 'columns', 'columns_laptop', 'columns_tablet', 'columns_phone' ) as $col ) {
 			$atts[ $col ] = ( 0 !== intval( $atts[ $col ] ) ) ? intval( $atts[ $col ] ) : 3;
 		}
 
@@ -309,7 +169,8 @@ class Cherry_Services_List_Shortcode {
 			'excerpt_length' => 'excerpt_length',
 			'col_xs'         => 'columns_phone',
 			'col_sm'         => 'columns_tablet',
-			'col_md'         => 'columns',
+			'col_md'         => 'columns_laptop',
+			'col_xl'         => 'columns',
 			'show_title'     => 'show_title',
 			'show_media'     => 'show_media',
 			'show_content'   => 'show_content',
